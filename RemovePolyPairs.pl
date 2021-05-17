c5607f912a108157eba88070884f5ab999e7e287
Teal Furnholm <tealfurnholm@gmail.com>
2021-05-17 13:38:54 -0400

    Update RemovePolyPairs.pl

--- RemovePolyPairs.pl
+++ RemovePolyPairs.pl
@@ -1,54 +1,61 @@
-use warnings;
+#use warnings;
 
-$input1 = $ARGV[0]; 
+$input1 = $ARGV[0];
 $input2 = $ARGV[1];
 $minlen = $ARGV[2];
 $output1 = $ARGV[3];
 $output2 = $ARGV[4];
 
-if($input1 !~ /(\_1\_|fwd)/i){ print "Please specify forward read file with either _1_ or _fwd_ in the file name\n"; }
-if($input2 !~ /(\_2\_|rev)/i){ print "Please specify reverse read file with either _2_ or _rev_ in the file name\n"; }
+if($input1 !~ /(\_1|fwd)/i){ print "Please specify forward read file with either _1_ or _fwd in the file name\n"; }
+if($input2 !~ /(\_2|rev)/i){ print "Please specify reverse read file with either _2_ or _rev in the file name\n"; }
 if($output1 !~ /\w/ || $output2 !~ /\w/ || $output1 eq $output2){ print "Please specify forward and reverse output file names\n"; }
 
 if($input1 =~ /\.gz$/i){
-	open(INPUT1, "gunzip -c $input1 |") or die "gunzip $input1: $!";
+        open(INPUT1, "gunzip -c $input1 |") or die "gunzip $input1: $!";
         open(INPUT2, "gunzip -c $input2 |") or die "gunzip $input2: $!";
 }
-else{	open(INPUT1, $input1)||die; open(INPUT2, $input2)||die; }
-
+else{   open(INPUT1, $input1)||die; open(INPUT2, $input2)||die; }
 if($output1 =~ /\.gz$/i){
-	open(OUTPUT1,'>>:gzip', $output1)||die;
-	open(OUTPUT2,'>>:gzip', $output2)||die;
+        open(OUTPUT1,'>:gzip', $output1)||die;
+        open(OUTPUT2,'>:gzip', $output2)||die;
 }
-else{	open(OUTPUT1, ">>", $output1)||die; open(OUTPUT2, ">>", $output2)||die; }
-
+else{   open(OUTPUT1, ">", $output1)||die;
+        open(OUTPUT2, ">", $output2)||die; }
+$log=$output1;
+$log=~s/\_*(fwd|for|1\_).*?$/_RPP.log/;
+open(LOG, ">>", $log)||die;
 
 $on=0;
 $badline=0;
 $start = localtime;
 while( my $line = <INPUT1> . <INPUT1> . <INPUT1> . <INPUT1> . <INPUT2> . <INPUT2> . <INPUT2> . <INPUT2> ) {
+        if($badline > 3){print "File parsing issue\n"; die;}
+        $totreads++;
+        $line = uc($line);
+        @stuff = split("\n", $line);
+        if($stuff[1] !~ /^[A-Z]+$/ || $stuff[5] !~ /^[A-Z]+$/){print "non-nucleotide detected on $on at $stuff[1] \n or \n $stuff[5]\n"; $badline++; next;}
+        $stuff[4] =~ /^(\S+)(\s|\_).*?/;
+        $read=$1;
+        if($stuff[0] !~ /$read/ || $read !~ /\w/){ print "paired name issues $stuff[0] ne $stuff[4] read $read badline $badline\n"; $badline++; next;}
+        if(exists($SEQS{$stuff[1]}{$stuff[5]})){$dup++; next;}
+        $save1 = $stuff[1]; $save3 = $stuff[3];
+        $save5 = $stuff[5]; $save7 = $stuff[7];
+        $len1 = length($stuff[1]);
+        $len2 = length($stuff[5]);
+        if($len1 < $minlen && $len2 < $minlen){$short++; next;} #both too short
 
-	$totreads++;
-   	$line = uc($line);
-	@stuff = split("\n", $line);
-	if($stuff[1] !~ /^[A-Z]+$/ || $stuff[5] !~ /^[A-Z]+$/){print "non-nucleotide detected on $on at $stuff[1] \n or \n $stuff[5]\n"; $badline++; next;}
-	$stuff[4]=~/^(\S+)\s/; $read=$1;
-	if($stuff[0] !~ /$read/){ print "paired fastq files not matched pairs $stuff[0] ne $stuff[4]\n"; $badline++; next;}
-	if($badline > 3){print "File parsing issue, make sure all lines in file match fastq format and pairs are matched\n"; die;}
-
-
-	#REMOVE AMBIGS
-	$stuff[1] =~ s/[^ATGC]/N/g;
-	if($stuff[1] =~ /^N|N$/){
-		@seq1 = split("", $stuff[1]);
-		@sco1 = split("", $stuff[3]);
-		while($seq1[0] eq "N"){ shift(@seq1); shift(@sco1); }
-		@seq1 = reverse(@seq1);
-		while($seq1[0] eq "N"){ shift(@seq1); shift(@sco1); }
-		@seq1 = reverse(@seq1);
-		$stuff[1] = join("", @seq1);
-		$stuff[3] = join("", @sco1);
-	}
+        #REMOVE AMBIGS
+        $stuff[1] =~ s/[^ATGC]/N/g;
+        if($stuff[1] =~ /^N|N$/){
+                @seq1 = split("", $stuff[1]);
+                @sco1 = split("", $stuff[3]);
+                while($seq1[0] eq "N"){ shift(@seq1); shift(@sco1); }
+                @seq1 = reverse(@seq1);
+                while($seq1[0] eq "N"){ shift(@seq1); shift(@sco1); }
+                @seq1 = reverse(@seq1);
+                $stuff[1] = join("", @seq1);
+                $stuff[3] = join("", @sco1);
+        }
         $stuff[5] =~ s/[^ATGC]/N/g;
         if($stuff[5] =~ /^N|N$/){
                 @seq2 = split("", $stuff[5]);
@@ -58,19 +65,77 @@ while( my $line = <INPUT1> . <INPUT1> . <INPUT1> . <INPUT1> . <INPUT2> . <INPUT2
                 while($seq2[0] eq "N"){ shift(@seq2); shift(@sco2); }
                 @seq2 = reverse(@seq2);
                 $stuff[5]=join("", @seq2);
-		$stuff[7] = join("", @sco2);
+                $stuff[7] = join("", @sco2);
         }
+        if(exists($SEQS{$stuff[1]}{$stuff[5]})){$dup++; next;}
+        my $NFs = $stuff[1] =~ tr/Nn/NN/;
+        my $NRs = $stuff[5] =~ tr/Nn/NN/;
+        if($NFs > $len1*0.05 && $NRs > $len2*0.05){ $ambig++; next;} #both too many ambigs
 
 
-	#REMOVE LOW ENTROPY READS
-	%DI = ();
-	@SEQ = split("", $stuff[1]);
-	for my $x (0..$#SEQ-1){
-	        $y = $x+1;
-	        $di = join("", @SEQ[$x..$y]);
-	        $DI{$di}=1;
-	}
-	$kc1 = keys %DI;
+        #REMOVE POLY AND SHORT REPEATS
+        $seq = $stuff[1];
+        $sco = $stuff[3];
+        while($seq =~ /^.?(A{30,}|T{30,}|G{30,}|C{30,}|(.{2})\2{14,}|(.{3})\3{9,}|(.{4})\4{6,}|(.{5})\5{5,}|(.{6})\6{4,})/){
+                $beg = @+[0];
+                @seq = split("", $seq);
+                @seq = @seq[$beg..$#seq];
+                @sco = split("", $sco);
+                @sco = @sco[$beg..$#sco];
+                $seq = join("", @seq);
+                $sco = join("", @sco);
+        }
+        while($seq =~ /(A{30,}|T{30,}|G{30,}|C{30,}|(.{2})\2{14,}|(.{3})\3{9,}|(.{4})\4{6,}|(.{5})\5{5,}|(.{6})\6{4,}).?$/){
+                $end = @-[0];
+                $end--;
+                @seq = split("", $seq);
+                @seq = @seq[0..$end];
+                @sco = split("", $sco);
+                @sco = @sco[0..$end];
+                $seq = join("", @seq);
+                $sco = join("", @sco);
+        }
+        $stuff[1] = $seq;
+        $stuff[3] = $sco;
+
+        #REMOVE POLY REVERSE
+        $seq = $stuff[5];
+        $sco = $stuff[7];
+        while($seq =~ /^.?(A{30,}|T{30,}|G{30,}|C{30,}|(.{2})\2{14,}|(.{3})\3{9,}|(.{4})\4{6,}|(.{5})\5{5,}|(.{6})\6{4,})/){
+                $beg = @+[0];
+                @seq = split("", $seq);
+                @seq = @seq[$beg..$#seq];
+                @sco = split("", $sco);
+                @sco = @sco[$beg..$#sco];
+                $seq = join("", @seq);
+                $sco = join("", @sco);
+        }
+        while($seq =~ /(A{30,}|T{30,}|G{30,}|C{30,}|(.{2})\2{14,}|(.{3})\3{9,}|(.{4})\4{6,}|(.{5})\5{5,}|(.{6})\6{4,}).?$/){
+                $end = @-[0];
+                $end--;
+                @seq = split("", $seq);
+                @seq = @seq[0..$end];
+                @sco = split("", $sco);
+                @sco = @sco[0..$end];
+                $seq = join("", @seq);
+                $sco = join("", @sco);
+        }
+        $stuff[5] = $seq;
+        $stuff[7] = $sco;
+        $len1 = length($stuff[1]);
+        $len2 = length($stuff[5]);
+        if($len1 < $minlen && $len2 < $minlen){$short++; next;} #both too short
+
+
+        #REMOVE LOW ENTROPY READS
+        %DI = ();
+        @SEQ = split("", $stuff[1]);
+        for my $x (0..$#SEQ-1){
+                $y = $x+1;
+                $di = join("", @SEQ[$x..$y]);
+                $DI{$di}=1;
+        }
+        $kc1 = keys %DI;
         %DI2 = ();
         @SEQ = split("", $stuff[5]);
         for my $x (0..$#SEQ-1){
@@ -79,117 +144,50 @@ while( my $line = <INPUT1> . <INPUT1> . <INPUT1> . <INPUT1> . <INPUT2> . <INPUT2
                 $DI2{$di}=1;
         }
         $kc2 = keys %DI2;
+        if($kc1 < 7 && $kc2 < 7){$lowent++; next;} #both low entropy
 
-	
-	#REMOVE POLY AND SHORT REPEATS
-	$seq = $stuff[1];
-	$sco = $stuff[3];
-	while($seq =~ /^.?A{20,}|T{20,}|G{20,}|C{20,}|(.{2,6})\1{5,}/){
-        if($seq =~ /^(A{20,}|T{20,}|G{20,}|C{20,})/){ $beg = @+[0]; }
-        elsif( $seq =~ /^(.{2,6})\1{5,}/){ $beg = @+[0]; }
-        elsif($seq =~ /^.?(A{20,}|T{20,}|G{20,}|C{20,})/){ $beg = @+[0]; }
-        elsif( $seq =~ /^.?(.{2,6})\1{5,}/){ $beg = @+[0]; }
-        else{last;}
-        $beg = @+[0];
-        @seq = split("", $seq);
-        @seq = @seq[$beg..$#seq];
-        @sco = split("", $sco);
-        @sco = @sco[$beg..$#sco];
-        $seq = join("", @seq);
-        $sco = join("", @sco);
-	}
-
-	while($seq =~ /A{20,}|T{20,}|G{20,}|C{20,}|(.{2,6})\1{5,}.?$/){
-        if($seq =~ /(A{20,}|T{20,}|G{20,}|C{20,})$/){ $beg = @+[0]; }
-        elsif( $seq =~ /(.{2,6})\1{5,}$/){ $beg = @+[0]; }
-        elsif($seq =~ /(A{20,}|T{20,}|G{20,}|C{20,}).?$/){ $beg = @+[0]; }
-        elsif( $seq =~ /(.{2,6})\1{5,}.?$/){ $beg = @+[0]; }
-        else{last;}
-        $end = @-[0];
-        $end--;
-        @seq = split("", $seq);
-        @seq = @seq[0..$end];
-        @sco = split("", $sco);
-        @sco = @sco[0..$end];
-        $seq = join("", @seq);
-        $sco = join("", @sco);
-	}
-	$stuff[1] = $seq;
-	$stuff[3] = $sco;
-
-
-	#REMOVE POLY REVERSE
-	$seq = $stuff[5];
-	$sco = $stuff[7];
-        while($seq =~ /^.?A{20,}|T{20,}|G{20,}|C{20,}|(.{2,6})\1{5,}/){
-        if($seq =~ /^(A{20,}|T{20,}|G{20,}|C{20,})/){ $beg = @+[0]; }
-        elsif( $seq =~ /^(.{2,6})\1{5,}/){ $beg = @+[0]; }
-        elsif($seq =~ /^.?(A{20,}|T{20,}|G{20,}|C{20,})/){ $beg = @+[0]; }
-        elsif( $seq =~ /^.?(.{2,6})\1{5,}/){ $beg = @+[0]; }
-        else{last;}
-        $beg = @+[0];
-        @seq = split("", $seq);
-        @seq = @seq[$beg..$#seq];
-        @sco = split("", $sco);
-        @sco = @sco[$beg..$#sco];
-        $seq = join("", @seq);
-        $sco = join("", @sco);
+        #OUTPUT READS
+        if($kc1 < 7 || $NFs > $len1*0.05 || $len1 < $minlen){ #forward read bad
+           if($kc2 < 7 || $NRs > $len2*0.05 || $len2 < $minlen){ $mixed++; next;} #both bad
+           $revgood++; $stuff[1]=$save1; $stuff[3]=$save3;
         }
-
-
-        while($seq =~ /A{20,}|T{20,}|G{20,}|C{20,}|(.{2,6})\1{5,}.?$/){
-        if($seq =~ /(A{20,}|T{20,}|G{20,}|C{20,})$/){ $beg = @+[0]; }
-        elsif( $seq =~ /(.{2,6})\1{5,}$/){ $beg = @+[0]; }
-        elsif($seq =~ /(A{20,}|T{20,}|G{20,}|C{20,}).?$/){ $beg = @+[0]; }
-        elsif( $seq =~ /(.{2,6})\1{5,}.?$/){ $beg = @+[0]; }
-        else{last;}
-        $end = @-[0];
-        $end--;
-        @seq = split("", $seq);
-        @seq = @seq[0..$end];
-        @sco = split("", $sco);
-        @sco = @sco[0..$end];
-        $seq = join("", @seq);
-        $sco = join("", @sco);
+        if($kc2 < 7 || $NRs > $len2*0.05 || $len2 < $minlen){ #reverse read bad
+           if($kc1 < 7 || $NFs > $len1*0.05 || $len1 < $minlen){ $mixed++; next;} #both bad
+           $forgood++; $stuff[5]=$save5; $stuff[7]=$save7;
         }
-        $stuff[5] = $seq;
-        $stuff[7] = $sco;
+        else{$bothgood++;}
 
 
-	#OUTPUT READS
-	my $NFs = $stuff[1] =~ tr/Nn/NN/; 
-	my $NRs = $stuff[5] =~ tr/Nn/NN/;
-	   if($kc1 < 7 && $kc2 < 7){													$lowent++; next;} #both low entropy
-	elsif($NFs > length($stuff[1])*0.05 && $NRs > length($stuff[5])*0.05){			$ambig++; next;} #both too many ambigs
-	elsif(length($stuff[1]) < $minlen && length($stuff[5]) < $minlen){				$short++; next;} #both too short
-	elsif($kc1 < 7 || $NFs > length($stuff[1])*0.05 || length($stuff[1]) < $minlen){#forward read bad
-	   if($kc2 < 7 || $NRs > length($stuff[1])*0.05 || length($stuff[5]) < $minlen){$mixed++; next;} #both bad
-	   $revgood++;
-	}
-	elsif($kc2 < 7 || $NRs > length($stuff[1])*0.05 || length($stuff[5]) < $minlen){#reverse read bad
-	   if($kc1 < 7 || $NFs > length($stuff[1])*0.05 || length($stuff[1]) < $minlen){$mixed++; next;} #both bad
-	   $forgood++;
-	}
-	else{$bothgood++;}
 
-	$stuff[0]=~s/\s.*//;
-	$SEQS{$stuff[1]}{$stuff[5]}=$stuff[0];
-	$SCOS{$stuff[1]}=$stuff[3];
-	$SCOS{$stuff[5]}=$stuff[7];
+        $SEQS{$stuff[1]}{$stuff[5]}=$read;
+        $SCOS{$stuff[1]}=$stuff[3];
+        $SCOS{$stuff[5]}=$stuff[7];
 
-	if($on%1000000==0){$time = localtime; print "on $on time $time tot $totreads bothgood $bothgood forgood $forgood revgood $revgood bothlowent $lowent bothambig $ambig bothshort $short mixed $mixed\n";} 
-	$on++;
+
+        if($on%1000000==0){$time = localtime;
+                print "on $on time $time tot $totreads dup $dup bothgood $bothgood forgood $forgood revgood $revgood bothlowent $lowent bothshort $short mixed $mixed $read\n";
+                print LOG "on $on time $time tot $totreads dup $dup bothgood $bothgood forgood $forgood revgood $revgood bothlowent $lowent bothshort $short mixed $mixed $read\n";
+        }
+        $on++;
 }
+
+
+foreach my $seq1 (sort(keys %SEQS)){
+        if($seq1 !~ /^[A-Z]+$/){$blr++; next;}
+        foreach my $seq2 (sort(keys %{$SEQS{$seq1}})){
+                if($seq2 !~ /^[A-Z]+$/){$brr++; next;}
+                $name = $SEQS{$seq1}{$seq2};
+                if($name !~ /\w/){$nn++; next; }
+                $nm1=$SEQS{$seq1}{$seq2}."_1";
+                $nm2=$SEQS{$seq1}{$seq2}."_2";
+                $totout++;
+                print OUTPUT1 "$nm1\n$seq1\n+\n$SCOS{$seq1}\n";
+                print OUTPUT2 "$nm2\n$seq2\n+\n$SCOS{$seq2}\n";
+        }
+}
+
 $end = localtime;
-print "on $on start $start end $end minlen $minlen\n";
-print "tot $totreads bothgood $bothgood forgood $forgood revgood $revgood bothlowent $lowent bothambig $ambig bothshort $short mixed $mixed\n";
+print LOG "$input1 $output1 on $on start $start end $end minlen $minlen tot $totreads totout $totout ";
+print LOG "noname $nn dup $dup bothgood $bothgood forgood $forgood revgood $revgood bothlowent $lowent bothambig $ambig bothshort $short mixed $mixed blr $blr brr $brr\n";
 
 
-foreach my $seq1 (keys %SEQS){
-	foreach my $seq2 (keys %{$SEQS{$seq1}}){
-		$nm1=$SEQS{$seq1}{$seq2}."_1";
-		$nm2=$SEQS{$seq1}{$seq2}."_2";
-		print OUTPUT1 "$nm1\n$seq1\n+\n$SCOS{$seq1}\n";
-		print OUTPUT2 "$nm2\n$seq2\n+\n$SCOS{$seq2}\n";
-	}
-}
